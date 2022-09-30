aebfa7e6b6c213a8bbd0d4c919d7876d10293ae7
JaafarHawli <jaafarhawle@gmail.com>
2022-09-30 19:37:24 +0300

    added comments to sortString function

--- app/Http/Controllers/ApisController.php
+++ app/Http/Controllers/ApisController.php
@@ -6,17 +6,26 @@ use Illuminate\Http\Request;
 
 class ApisController extends Controller
 {
+    // Sort string function: Sorts any string in this form "aAbB...zZ12...9"
+    // Note: Can be implemented in a more efficient way 
     function sortString($string) {
         $sorted = "";
         $lower = "";
         $capital = "";
 
+        // Ascii value of letter "a"
         $small_letter = 97;
+        // Ascii value of letter "A"
         $capital_letter = 65;
+        // Ascii value of number "0"
         $number_asci = 48;
+        // Length of the input string
         $length = strlen($string);
 
+        // This loop sorts the characters first "aAbB...zZ"
+        // Fisrt loop to iterate over every letter in the alphabet in order
         for($j=0; $j<26; $j++) {
+            // Second loop tpo iterate over every character in the string and compare it to the current alphabatical character
             for($i=0; $i<$length; $i++) {
                 $asci = ord($string[$i]);
                 if($asci == $small_letter) {
@@ -33,7 +42,7 @@ class ApisController extends Controller
             $capital = ""; 
         }
         
-        
+        // This loop sorts the numbers inside the string and adds them to the end of the sorted string
         for($k=0; $k<10;$k++) {
             for($z=0; $z<$length; $z++) {
                 $asci = ord($string[$z]);
