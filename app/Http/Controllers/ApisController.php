e87ef1bc6e07c13606a9aeff7460931103f2078b
JaafarHawli <jaafarhawle@gmail.com>
2022-09-30 18:59:19 +0300

    modified sortString function: the function returns 'aAbB...zZ' if found

--- app/Http/Controllers/ApisController.php
+++ app/Http/Controllers/ApisController.php
@@ -15,16 +15,23 @@ class ApisController extends Controller
         $capital_letter = 65;
         $length = strlen($string);
 
-        for($i=0; $i<$length; $i++) {
-            $asci = ord($string[$i]);
-            if($asci == $small_letter) {
-                $lower .= $string[$i];
+        for($j=0; $j<26; $j++) {
+            for($i=0; $i<$length; $i++) {
+                $asci = ord($string[$i]);
+                if($asci == $small_letter) {
+                    $lower .= $string[$i];
+                }
+                else if($asci == $capital_letter) {
+                    $capital .= $string[$i];
+                }
             }
-            else if($asci == $capital_letter) {
-                $capital .= $string[$i];
-            }
+            $sorted .= $lower.$capital;
+            $small_letter +=1;
+            $capital_letter+=1;
+            $lower = "";
+            $capital = "";
         }
-        $sorted = $lower.$capital;
+        
 
         return response()->json([
             $string => $sorted
