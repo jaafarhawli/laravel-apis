7ce07725365dbde08404eee2aff3744a28a51480
JaafarHawli <jaafarhawle@gmail.com>
2022-09-30 18:53:31 +0300

    added sortString funtion: the function returns 'aA' in order if found

--- app/Http/Controllers/ApisController.php
+++ app/Http/Controllers/ApisController.php
@@ -6,5 +6,28 @@ use Illuminate\Http\Request;
 
 class ApisController extends Controller
 {
-    
+    function sortString($string) {
+        $sorted = "";
+        $lower = "";
+        $capital = "";
+
+        $small_letter = 97;
+        $capital_letter = 65;
+        $length = strlen($string);
+
+        for($i=0; $i<$length; $i++) {
+            $asci = ord($string[$i]);
+            if($asci == $small_letter) {
+                $lower .= $string[$i];
+            }
+            else if($asci == $capital_letter) {
+                $capital .= $string[$i];
+            }
+        }
+        $sorted = $lower.$capital;
+
+        return response()->json([
+            $string => $sorted
+        ]);
+    } 
 }
